389e06f50de895555f83cabd3e6fae72d0101f81
Filipe Gorges Reuwsaat <filipe.gorges.reuwsaat@gmail.com>
2023-07-27 10:08:44 -0300

    initial commit

---
+++ src/modules/@shared/domain/value-object/id.value-object.ts
@@ -0,0 +1,15 @@
+import ValueObject from "./value-object.interface";
+import { v4 as uuid } from 'uuid';
+
+
+export default class Id implements ValueObject {
+    private readonly _id: string;
+
+    constructor(id?: string) {
+        this._id = id || uuid();
+    }
+
+    get value(): string {
+        return this._id;
+    }
+}
